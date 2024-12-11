fc0b2b7ecc1ab40af3589caaae61017d31c84467
Nahid Hasan <nahidn228@gmail.com>
2024-12-11 19:32:45 +0600

    get all jobs data in /jobs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
+require("dotenv").config();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -16,14 +17,10 @@ app.listen(port, () => {
   console.log(`Job Portal app listening on port ${port}`);
 });
 
-
 // jobPortal
 // rHiMAV2LLpuqHNoP
 
-
-
-
-const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.dssil.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dssil.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -31,19 +28,29 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. JobPortal successfully connected to MongoDB!"
+    );
+
+    //Jobs related API
+    const jobsCollection = client.db("jobPortal").collection("jobs");
+    app.get("/jobs", async (req, res) => {
+      const cursor = jobsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);

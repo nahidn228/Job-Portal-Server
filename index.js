9d4d80751dfbd646d82a9dc868d08056ccfa894c
Nahid Hasan <nahidn228@gmail.com>
2024-12-12 22:43:32 +0600

    Show My Posted Jobs And Count Job Applications

--- index.js
+++ index.js
@@ -79,6 +79,30 @@ async function run() {
     app.post("/job-applications", async (req, res) => {
       const application = req.body;
       const result = await jobApplicationCollection.insertOne(application);
+
+      //not the best way(use aggregate)
+      //optional can skip
+      const id = application.job_id;
+      const query = { _id: new ObjectId(id) };
+      const job = await jobsCollection.findOne(query);
+
+      let newCount = 0;
+      if (job.applicationCount) {
+        newCount = job.applicationCount + 1;
+      } else {
+        newCount = 1;
+      }
+
+      //now update the job info
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          applicationCount: newCount,
+        },
+      };
+
+      const updatedResult = await jobsCollection.updateOne(filter, updatedDoc);
+
       res.send(result);
     });
 
@@ -86,6 +110,7 @@ async function run() {
       const email = req.query.email;
       const query = { applicant_email: email };
       const result = await jobApplicationCollection.find(query).toArray();
+
       //Fokira way to aggregate  data
       for (const application of result) {
         console.log(application.job_id);
